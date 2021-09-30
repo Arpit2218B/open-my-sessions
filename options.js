0bb8fe6c94529af7325508bc71e1ec4925f2e7be
Arpit Rathi <arpit97rathi@gmail.com>
2021-09-30 16:45:07 +0530

    added outline for scripts

---
+++ options.js
@@ -0,0 +1,21 @@
+// Get all elements
+    // Save button
+    // Open Button
+
+// Add constants - includs list item templates
+
+// Add event handler functions
+    // populateList
+    // save
+    // open
+    // removeFromList
+
+// Utility function
+    // Add to chrome storage
+    // Remove from chrome storage
+
+// Add event hadlers to elements
+
+// init function
+
+// call init function
